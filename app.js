e2028848dfa4f821786e5366f54566f034de722d
Max Wang <wangm13@rpi.edu>
2018-03-01 01:38:23 -0500

    added Create Meeting functionality

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('etag', false);
 
 var port = process.env.PORT || 3000;
 
-// sets up a pool of 10 connections to the DB
+sets up a pool of 10 connections to the DB
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : process.env.RDS_HOSTNAME,
@@ -22,6 +22,15 @@ var pool = mysql.createPool({
     password        : process.env.RDS_PASSWORD,
     port            : process.env.RDS_PORT
 });
+
+// var pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host: "aa1bp2i9g06gxma.cuqrdbzoxw7e.us-east-1.rds.amazonaws.com",
+//     user: "meetingScheduler",
+//     password: "tacocat42",
+//     port: "3306"
+
+// });
 
 // test the connection to the DB
 var testConnection = new Promise(function(resolve, reject) {
@@ -56,6 +65,44 @@ app.get('/dbStatus', function (req, res) {
 var server = app.listen(port, function () {
     console.log('Server running at http://127.0.0.1:' + port + '/');
 });
+
+//initial meeting endpoints
+
+//create a meeting
+app.post("/meeting", function (req, res) {
+    var start_datetime = req.query.start_datetime;
+    var end_datetime = req.query.end_datetime;
+    var room_number = req.query.room_number;
+
+    console.log(req);
+
+    var sql = "INSERT INTO ebdb.Meeting (start_datetime, end_datetime, room_number) VALUES (?, ?, ?);"
+    var inserts = [start_datetime, end_datetime, room_number];
+    mysql.format(sql, inserts);
+
+    console.log(mysql.format(sql, inserts));
+
+    pool.getConnection(function(err,connection) {
+        connection.query(sql, inserts, function(error, results, fields) {
+            console.log(results);
+            console.log(error);
+            console.log(fields);
+        });
+    });
+
+});
+
+// app.get("/meeting/:meetingId", function (req, res) {
+    
+// });
+
+// app.put("/meeting/:meetingId", function (req, res) {
+    
+// });
+
+// app.delete("/meeting/:meetingId", function (req, res) {
+    
+// });
 
 function cleanup() {
     console.log("shutting down");
