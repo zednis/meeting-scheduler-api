42da5e5ba2fcd8c2c066624057e05b74781b3df6
Roshni <roshni.vachhani96@gmail.com>
2018-04-21 14:17:26 -0400

    Updated Meeting information loading

--- calendar.js
+++ calendar.js
@@ -1,3 +1,4 @@
+
 var app = angular.module("CalendarApp", []);
 
 app.controller('CalendarCtrl', function($scope, $http){
@@ -19,9 +20,54 @@ app.controller('CalendarCtrl', function($scope, $http){
 				url = url + "/" + $scope.userID + '/meetings';
 
 				$http.get(url, {params:$scope.userID}).then(function(response){
-					$scope.meetings = response.data.meetings;
-				});	
+					var meetings = response.data.meetings;
+					//Parse meetings array and form events array that will be used in the calendar interface
+					$scope.events = []
+					for (i=0; i < meetings.length; i++){
+						temp = {
+							title: meetings[i].name,
+							start: meetings[i].startDateTime,
+							end: meetings[i].endDateTime
+						}
+						$scope.events.push(temp);
+					}
+					loadCalendar($scope.events);
+				}); //end of meetings function	
 			}
 		});
 	}; //end of search function
+}); //end of angular app
+
+function loadCalendar(myEvents) {
+	console.log("Function was called");
+	console.log(myEvents);
+	$('#calendar').fullCalendar({
+	  header: {
+	    left: 'prev,next today',
+	    center: 'title',
+	    right: 'month,basicWeek,basicDay'
+	  },
+	  defaultDate: Date(),
+	  navLinks: true, // can click day/week names to navigate views
+	  editable: true,
+	  eventLimit: true, // allow "more" link when too many events
+	  events: myEvents
+	});
+	$('#calendar').reload(true);
+};
+
+$(document).ready(function() {
+	console.log("Ran main calendar function");
+	$('#calendar').fullCalendar({
+	  header: {
+	    left: 'prev,next today',
+	    center: 'title',
+	    right: 'month,basicWeek,basicDay'
+	  },
+	  defaultDate: Date(),
+	  navLinks: true, // can click day/week names to navigate views
+	  editable: true,
+	  eventLimit: true, // allow "more" link when too many events
+	  events: []
+	});
 });
